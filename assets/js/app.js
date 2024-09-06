8e67b245a0b830cf180dd1f2228dbac7360d8566
BimlaJain <vj13798@gmail.com>
2024-09-06 11:05:25 +0530

    change js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
-const cookiePopup = document.getElementById('cookie-popup');
-const acceptCookiesButton = document.getElementById('accept-cookies');
-const declineCookiesButton = document.getElementById('decline-cookies');
 
-
-acceptCookiesButton.addEventListener('click', () => {
-    localStorage.setItem('accepted', 'true');
-    cookiePopup.classList.add('d-none');
-});
-
-declineCookiesButton.addEventListener('click', () => {
-    localStorage.setItem('denied', 'true');
-    cookiePopup.classList.add('d-none');
+document.addEventListener('DOMContentLoaded', function () {
+    const cookiePopup = document.getElementById('cookie-popup');
+    const acceptBtn = document.getElementById('accept-cookies');
+    const declineBtn = document.getElementById('decline-cookies');
+    if (!localStorage.getItem('cookiesConsent')) {
+        cookiePopup.style.display = 'block';
+    } else {
+        cookiePopup.style.display = 'none';
+    }
+    acceptBtn.addEventListener('click', function () {
+        localStorage.setItem('cookiesConsent', 'accepted');
+        cookiePopup.style.display = 'none';
+    });
+    declineBtn.addEventListener('click', function () {
+        localStorage.setItem('cookiesConsent', 'denied');
+        cookiePopup.style.display = 'none';
+    });
 });
